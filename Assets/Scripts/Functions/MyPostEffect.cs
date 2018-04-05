ac5cf4b8d5559fea8e4f7bb15c16cdbda1a624c4
Minstreams <minstreams@qq.com>
2018-04-05 18:24:58 +0800

    自己的后处理特效cs脚本

---
+++ Assets/Scripts/Functions/MyPostEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 后处理特效脚本
+/// </summary>
+[AddComponentMenu("Function/My Post Effect")]
+public class MyPostEffect : UnityStandardAssets.ImageEffects.PostEffectsBase {
+    [Header("【后处理特效脚本】")]
+    [Header("后处理材质")]
+    public Material mat;
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if(mat == null)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+        Graphics.Blit(source, destination, mat);
+    }
+}
